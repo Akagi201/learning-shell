1a39fae13f52ff599e54adc74ada8dc0f88a00b6
Akagi201 <akagi201@gmail.com>
2016-05-31 11:33:54 +0800

    add default_val

---
+++ bash/variable/default_val.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# default val
+NUM_JOBS=${NUM_JOBS:-1}
+
+echo $NUM_JOBS
+
+# set val
+
+NUM_JOBS1=2
+
+NUM_JOBS1=${NUM_JOBS1:-1}
+
+echo $NUM_JOBS1
+
+# default val
+
+NUM_JOBS2=${NUM_JOBS2:=1}
+
+echo $NUM_JOBS2
+
+# set val
+
+NUM_JOBS3=2
+
+NUM_JOBS3=${NUM_JOBS3:=1}
+
+echo $NUM_JOBS3
+
